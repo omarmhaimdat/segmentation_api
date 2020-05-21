a29b27f123d51cb31e9ac5133906372aecdeff8f
Omar MHAIMDAT <omarmhaimdat@gmail.com>
2020-05-21 23:01:45 +0000

    Initial Commit

---
+++ segmentation_api/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  segmentation_api
+//
+//  Created by M'haimdat omar on 21-05-2020.
+//  Copyright © 2020 M'haimdat omar. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
